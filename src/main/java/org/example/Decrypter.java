7043ff3866abfcc6bf7ef5193607304754a0c684
Jakob Silbereisen <jakob.silbereisen@foconis.de>
2025-06-11 11:30:47 +0200

    Implement

--- src/main/java/org/example/Decrypter.java
+++ src/main/java/org/example/Decrypter.java
@@ -1,10 +1,32 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Jakob Silbereisen, Foconis Analytics GmbH
  */
 public class Decrypter {
+
+	private static final Map<Character, Character> DECRYPT_MAP = new HashMap<>();
+
+	public Decrypter() {
+		String letters = "abcdefghijklmnopqrstuvwxyz ";
+		String symbols = "!)\"(£*%&><@abcdefghijklmno ";
+
+		for (int i = 0; i < letters.length(); i++) {
+			DECRYPT_MAP.put(symbols.charAt(i), letters.charAt(i));
+		}
+	}
+
 	public String decrypt(String encryped) {
-		return null;
+		StringBuilder result = new StringBuilder();
+		for (char c : encryped.toCharArray()) {
+			Character translater = DECRYPT_MAP.get(c);
+			if (translater != null) {
+				result.append(translater);
+			}
+		}
+		return result.toString();
 	}
 }
