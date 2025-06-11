372611457a3d9af5718df01598bb33aa92d1d97b
Jakob Silbereisen <jakob.silbereisen@foconis.de>
2025-06-11 11:42:10 +0200

    implement encryption

--- src/main/java/org/example/Decrypter.java
+++ src/main/java/org/example/Decrypter.java
@@ -9,6 +9,7 @@ import java.util.Map;
 public class Decrypter {
 
 	private static final Map<Character, Character> DECRYPT_MAP = new HashMap<>();
+	private static final Map<Character, Character> ENCRYPT_MAP = new HashMap<>();
 
 	public Decrypter() {
 		String letters = "abcdefghijklmnopqrstuvwxyz ";
@@ -16,6 +17,7 @@ public class Decrypter {
 
 		for (int i = 0; i < letters.length(); i++) {
 			DECRYPT_MAP.put(symbols.charAt(i), letters.charAt(i));
+			ENCRYPT_MAP.put(letters.charAt(i), symbols.charAt(i));
 		}
 	}
 
@@ -33,6 +35,15 @@ public class Decrypter {
 	}
 
 	public String encrypt(String deprypted) {
-		return null;
+		StringBuilder result = new StringBuilder();
+		for (char c : deprypted.toCharArray()) {
+			Character translater = ENCRYPT_MAP.get(c);
+			if (translater != null) {
+				result.append(translater);
+			} else {
+				throw new IllegalArgumentException("No translation for character " + c);
+			}
+		}
+		return result.toString();
 	}
 }
